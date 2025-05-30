94d93b67443845ac4325f597cd59b1dae7fb23d3
Kartik Namdev <kartikk12na@gmail.com>
2025-05-30 23:11:32 +0530

    added an alternate frontend url

--- src/app.js
+++ src/app.js
@@ -13,7 +13,10 @@ const app = express();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "https://peertask.vercel.app",
+    origin: [
+      process.env.FRONTEND_URL || "https://peertask.vercel.app","https://peer-taskk.vercel.app",
+      "http://localhost:5173"
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
